72f7f58608d1ad9102ba63cbb3d45e48093457d6
Julio Carvajal <julio.carvajal@enter.co>
2016-05-15 20:44:54 -0500

    Ajustes iniciales de la plantilla

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,9 +1,47 @@
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+<!--<![endif]-->
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="language" content="es">
+	<!-- Basic Page Needs
+    ================================================== -->
+	<meta name="description" content="Proyecto de aprendizaje AKO">
+    <meta name="author" content="Grupo Universidad EAN">
+	<!-- Mobile Specific Metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- CSS
+    ================================================== -->
+    <!-- Bootstrap  -->
+    <link type="text/css" rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/css/bootstrap.min.css">
+    <!-- web font  -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,800" rel="stylesheet" type="text/css">
+    <!-- plugin css  -->
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/js-plugin/animation-framework/animate.css" />
+    <link rel="stylesheet" type="text/css" href="js-plugin/pretty-photo/css/prettyPhoto.css" />
+    <link rel="stylesheet" type="text/css" href="js-plugin/flexslider/flexslider.css" />
+    <!-- icon fonts -->
+    <link type="text/css" rel="stylesheet" href="font-icons/custom-icons/css/custom-icons.css">
+    <link type="text/css" rel="stylesheet" href="font-icons/custom-icons/css/custom-icons-ie7.css">
+    <!-- Custom css -->
+    <link type="text/css" rel="stylesheet" href="css/layout.css">
+    <link type="text/css" id="colors" rel="stylesheet" href="css/green.css">
+    <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script> <![endif]-->
+    <script src="js/modernizr-2.6.1.min.js"></script>
+	<!-- Favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="images/apple-touch-icon-144x144.png">
 
 	<!-- blueprint CSS framework -->
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection">
@@ -55,6 +93,24 @@
 	</div><!-- footer -->
 
 </div><!-- page -->
-
+<!-- End Document 
+    ================================================== -->
+    <script type="text/javascript" src="js-plugin/respond/respond.min.js"></script>
+    <script type="text/javascript" src="js-plugin/jquery/1.8.3/jquery.min.js"></script>
+    <script type="text/javascript" src="js-plugin/jquery-ui/jquery-ui-1.8.23.custom.min.js"></script>
+    <!-- third party plugins  -->
+    <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap-carousel-ie.js"></script>
+    <script type="text/javascript" src="js-plugin/easing/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="js-plugin/pretty-photo/js/jquery.prettyPhoto.js"></script>
+    <script type="text/javascript" src="js-plugin/flexslider/jquery.flexslider-min.js"></script>
+    <script type="text/javascript" src="js-plugin/jquery.sharrre-1.3.4/jquery.sharrre-1.3.4.min.js"></script>
+    <script type="text/javascript" src="js-plugin/neko-contact-ajax-plugin/js/jquery.form.js"></script>
+    <script type="text/javascript" src="js-plugin/neko-contact-ajax-plugin/js/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="js-plugin/parallax/js/jquery.scrollTo-1.4.3.1-min.js"></script>
+    <script type="text/javascript" src="js-plugin/parallax/js/jquery.localscroll-1.2.7-min.js"></script>
+    <script type="text/javascript" src="js-plugin/parallax/js/jquery.stellar.min.js"></script>
+    <!-- Custom  -->
+    <script type="text/javascript" src="js/custom.js"></script>
 </body>
 </html>
